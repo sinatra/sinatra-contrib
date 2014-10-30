811a97117374dba7ee18bf53a59a97c1a27efc71
Andrea Leopardi <an.leopardi@gmail.com>
2014-10-30 17:17:41 +0100

    Use Sinatra's logger in Sinatra::ConfigFile
    Before this commit, `ConfigFile` just logged to stderr with `$stderr.puts`. Now,
    it logs using Sinatra's `logger` helper.

--- lib/sinatra/config_file.rb
+++ lib/sinatra/config_file.rb
@@ -127,7 +127,7 @@ module Sinatra
       Dir.chdir(root || '.') do
         paths.each do |pattern|
           Dir.glob(pattern) do |file|
-            $stderr.puts "loading config file '#{file}'" if logging?
+            logger.info "Loading configuration file '#{file}'"
             document = IO.read(file)
             document = ERB.new(document).result if file.split('.').include?('erb')
             yaml = config_for_env(YAML.load(document)) || {}
